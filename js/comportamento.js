8e6b61eb7fd41068171e694cd7f02c06e81aeae3
Roger Bernardo <rogerbernardo007@gmail.com>
2019-11-07 10:53:10 -0300

    Alterando estrutura do código em javascript

--- js/comportamento.js
+++ js/comportamento.js
@@ -6,36 +6,43 @@ Preencher na matriz abaixo os seguintes números:
 1 = representa o Jogador 1
 2 = representa o jogador 2 (ou a Máquina)
 
-matriz[linha][coluna]
+matriz[posição]
+
+    0 1 2
+    3 4 5
+    6 7 8
 */
 
 /* ---------->>> VARIÁVEIS GLOBAIS <<<----------*/
-var rodada = 0;
+var rodada = 1;
+var n_jogos = 1;
+var jogabilidade = true;
+var vez_jogador = 0;
 
 /* ---------->>> MATRIZ DO JOGO <<<----------*/
-var matriz = new Array(3);
-for (var i = 0; i < 3; i++)
-    matriz[i] = new Array(3);
-
-for (var i = 0; i < 3; i++)
-    for (var j = 0; j < 3; j++)
-        matriz[i][j] = 0;
+var matriz = new Array(9);
+for (var i = 0; i < 9; i++)
+    matriz[i] = 0;
 
 
-
-function jogar(id, x, y) {
-    rodada++;
-
-    if ((rodada % 2) == 1) {
-        //jogada do jogador 1
-        document.getElementById(id).style.backgroundImage = "url('img/x.png')";
-        matriz[x][y] = 1;
+function jogar(id, posicao) {
+    vez_jogador = rodada % 2;
+    if (vez_jogador == 1 && jogabilidade == true) {
+        //jogada do jogador 1       
+        if (matriz[posicao] == 0) {
+            document.getElementById(id).style.backgroundImage = "url('img/x.png')";
+            matriz[posicao] = 1;
+            rodada++;
+        }
     }
 
-    else {
+    if (vez_jogador == 0 && jogabilidade == true) {
         //jogada do jogador 2
-        document.getElementById(id).style.backgroundImage = "url('img/o.png')";
-        matriz[x][y] = 2;
+        if (matriz[posicao] == 0) {
+            document.getElementById(id).style.backgroundImage = "url('img/o.png')";
+            matriz[posicao] = 2;
+            rodada++;
+        }
     }
 
     vitoria1();
@@ -45,90 +52,107 @@ function jogar(id, x, y) {
 
 function vitoria1() {
     //Horizontal
-    if (matriz[0][0] == 1 && matriz[1][0] == 1 && matriz[2][0] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[0] == 1 && matriz[1] == 1 && matriz[2] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][1] == 1 && matriz[1][1] == 1 && matriz[2][1] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[3] == 1 && matriz[4] == 1 && matriz[5] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][2] == 1 && matriz[1][2] == 1 && matriz[2][2] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[6] == 1 && matriz[7] == 1 && matriz[8] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
     //Vertical
-    if (matriz[0][0] == 1 && matriz[0][1] == 1 && matriz[0][2] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[0] == 1 && matriz[3] == 1 && matriz[6] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[1][0] == 1 && matriz[1][1] == 1 && matriz[1][2] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[1] == 1 && matriz[4] == 1 && matriz[7] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[2][0] == 1 && matriz[2][1] == 1 && matriz[2][2] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[2] == 1 && matriz[5] == 1 && matriz[8] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
     //Diagonal
-    if (matriz[0][0] == 1 && matriz[1][1] == 1 && matriz[1][2] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[0] == 1 && matriz[4] == 1 && matriz[8] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][2] == 1 && matriz[1][1] == 1 && matriz[2][0] == 1) {
-        console.log("Jogador 1 venceu!");
+    if (matriz[2] == 1 && matriz[4] == 1 && matriz[6] == 1) {
+        console.log("Jogador 1 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 }
 
 function vitoria2() {
     //Horizontal
-    if (matriz[0][0] == 2 && matriz[1][0] == 2 && matriz[2][0] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[0] == 2 && matriz[1] == 2 && matriz[2] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][1] == 2 && matriz[1][1] == 2 && matriz[2][1] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[3] == 2 && matriz[4] == 2 && matriz[5] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][2] == 2 && matriz[1][2] == 2 && matriz[2][2] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[6] == 2 && matriz[7] == 2 && matriz[8] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
     //Vertical
-    if (matriz[0][0] == 2 && matriz[0][1] == 2 && matriz[0][2] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[0] == 2 && matriz[3] == 2 && matriz[6] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[1][0] == 2 && matriz[1][1] == 2 && matriz[1][2] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[1] == 2 && matriz[4] == 2 && matriz[7] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[2][0] == 2 && matriz[2][1] == 2 && matriz[2][2] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[2] == 2 && matriz[5] == 2 && matriz[8] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
     //Diagonal
-    if (matriz[0][0] == 2 && matriz[1][1] == 2 && matriz[1][2] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[0] == 2 && matriz[4] == 2 && matriz[8] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 
-    if (matriz[0][2] == 2 && matriz[1][1] == 2 && matriz[2][0] == 2) {
-        console.log("Jogador 2 venceu!");
+    if (matriz[2] == 2 && matriz[4] == 2 && matriz[6] == 2) {
+        console.log("Jogador 2 venceu no jogo nº: " + n_jogos);
+        jogabilidade = false;
     }
 }
 
+
 function novoJogo() {
-    rodada = 0;
+    rodada = 1;
+    n_jogos++;
+    jogabilidade = true;
 
     var campos = document.getElementsByClassName("default");
     for (var i = 0; i < campos.length; i++) {
         campos[i].style.backgroundImage = "none";
     }
 
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            matriz[i][j] = 0;
-        }
+    for (var i = 0; i < 9; i++) {
+        matriz[i] = 0;
     }
 }
 
